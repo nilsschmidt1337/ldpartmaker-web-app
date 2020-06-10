516deba0878f66975809d6175f3151f835491d49
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2020-06-10 19:13:06 +0200

    Reactivated CI tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,8 +27,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    // browsers: ['ChromeHeadless'], requires: npm i -D puppeteer
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
