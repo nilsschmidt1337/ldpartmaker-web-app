f65e06f24ff447f6a4ac874a415a40f7389de235
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2020-06-21 13:03:23 +0200

    Wrote a test for the native file system API check.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,18 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ldpartmaker-web-app');
   });
 
-  it('should have a canvas', () => {
+  it('should have a canvas when the native file system api is supported', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.componentInstance.hasNativeFS = true;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('canvas').textContent).toContain('Your browser does not support the <canvas> element.');
+  });
+
+  it('should have a div when the native file system api is not supported', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('canvas').textContent).toContain('Your browser does not support');
+    expect(compiled.querySelector('#not-supported').textContent).toContain('Native File System API');
   });
 });
